8efb62809e5b943604002ea9ffae581a81e70681
duynguyen19051997 <nguyenvuanhduy1905@gmail.com>
2020-01-07 10:08:16 +0700

    Learn SpringBoot

---
+++ getting-started/src/main/java/duynguyen/springboot/controller/MainController.java
@@ -0,0 +1,61 @@
+package duynguyen.springboot.controller;
+
+import duynguyen.springboot.model.bean.Person;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Controller
+public class MainController {
+    private static List<Person> persons = new ArrayList<>();
+
+    static {
+        persons.add(new Person("Bill", "Gates"));
+        persons.add(new Person("Steve", "Jobs"));
+    }
+
+    @Value("${welcome.message}")
+    private String message;
+
+    @Value("${error.message}")
+    private String errorMessage;
+
+    @GetMapping({"/", "/index"})
+    public String index(Model model) {
+        model.addAttribute("message", message);
+        return "index";
+    }
+
+    @GetMapping("/person-list")
+    public String personList(Model model) {
+        model.addAttribute("persons", persons);
+        return "person-list";
+    }
+
+    @GetMapping("/add-person")
+    public String showAddPersonPage(ModelMap modelMap) {
+        Person personForm = new Person();
+        modelMap.addAttribute("personForm", personForm);
+        return "add-person";
+    }
+
+    @PostMapping("/add-person")
+    public String savePerson(ModelMap modelMap, //
+                             @ModelAttribute("personForm") Person person) {
+        String firstName = person.getFirstName();
+        String lastName = person.getLastName();
+        if (firstName != null && firstName.length() > 0 //
+                && lastName != null && lastName.length() > 0) {
+            Person newPerson = new Person(firstName, lastName);
+            persons.add(newPerson);
+            return "redirect:/person-list";
+        }
+        modelMap.addAttribute("errorMessage", errorMessage);
+        return "add-person";
+    }
+}
---
+++ getting-started/src/main/java/duynguyen/springboot/model/bean/Person.java
@@ -0,0 +1,13 @@
+package duynguyen.springboot.model.bean;
+
+import lombok.*;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@ToString
+@NoArgsConstructor
+public class Person {
+    private String firstName;
+    private String lastName;
+}
