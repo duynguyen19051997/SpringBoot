1c4a17592e659518d585445a7177980158f80f90
duynguyen19051997 <nguyenvuanhduy1905@gmail.com>
2020-03-03 21:24:31 +0700

    Spring Boot i18n

--- internationalization/src/main/java/duynguyen/internationalization/config/WebMvcConfig.java
+++ internationalization/src/main/java/duynguyen/internationalization/config/WebMvcConfig.java
@@ -7,25 +7,12 @@ import org.springframework.context.support.ReloadableResourceBundleMessageSource
 import org.springframework.web.servlet.LocaleResolver;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-import org.springframework.web.servlet.i18n.CookieLocaleResolver;
-import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
 
 @Configuration
 public class WebMvcConfig implements WebMvcConfigurer {
-
-
-    @Bean(name = "localeResolver")
-    public LocaleResolver getLocaleResolver()  {
-        CookieLocaleResolver resolver= new CookieLocaleResolver();
-        resolver.setCookieDomain("myAppLocaleCookie");
-        // 60 minutes
-        resolver.setCookieMaxAge(60*60);
-        return resolver;
-    }
-
     @Bean(name = "messageSource")
-    public MessageSource getMessageResource()  {
-        ReloadableResourceBundleMessageSource messageResource= new ReloadableResourceBundleMessageSource();
+    public MessageSource getMessageResource() {
+        ReloadableResourceBundleMessageSource messageResource = new ReloadableResourceBundleMessageSource();
 
         // Read i18n/messages_xxx.properties file.
         // For example: i18n/messages_en.properties
@@ -33,13 +20,23 @@ public class WebMvcConfig implements WebMvcConfigurer {
         messageResource.setDefaultEncoding("UTF-8");
         return messageResource;
     }
+
+    // To solver URL like:
+    // /SomeContextPath/en/login2
+    // /SomeContextPath/vi/login2
+    // /SomeContextPath/fr/login2
+    @Bean(name = "localeResolver")
+    public LocaleResolver getLocaleResolver() {
+        LocaleResolver resolver = new UrlLocaleResolver();
+        return resolver;
+    }
+
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
-        LocaleChangeInterceptor localeInterceptor = new LocaleChangeInterceptor();
-        localeInterceptor.setParamName("lang");
 
+        UrlLocaleInterceptor localeInterceptor = new UrlLocaleInterceptor();
 
-        registry.addInterceptor(localeInterceptor).addPathPatterns("/*");
+        registry.addInterceptor(localeInterceptor).addPathPatterns("/en/*", "/fr/*", "/vi/*");
     }
 
 }
--- internationalization/src/main/java/duynguyen/internationalization/controller/MainController.java
+++ internationalization/src/main/java/duynguyen/internationalization/controller/MainController.java
@@ -5,6 +5,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
@@ -21,8 +22,8 @@ public class MainController {
         return "success";
     }
 
-    @GetMapping("login")
-    public String home() {
+    @GetMapping({"login", "/{locale:en|fr|vn}/login"})
+    public String home(@PathVariable String locale) {
         return "login";
     }
 
