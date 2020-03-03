dab20ae7e1ded650ee93413ff8e2f261a501485d
duynguyen19051997 <nguyenvuanhduy1905@gmail.com>
2020-03-03 19:59:33 +0700

    internationalization

---
+++ internationalization/src/test/java/duynguyen/internationalization/InternationalizationApplicationTests.java
@@ -0,0 +1,13 @@
+package duynguyen.internationalization;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InternationalizationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
