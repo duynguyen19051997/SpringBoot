3f37b894585a4876db8c3741c88cc485442f6363
duynguyen19051997 <nguyenvuanhduy1905@gmail.com>
2020-03-04 22:41:40 +0700

    Spring Boot Interceptors

---
+++ interceptors/src/main/java/duynguyen/interceptors/config/WebMvcConfig.java
@@ -0,0 +1,31 @@
+package duynguyen.interceptors.config;
+
+import duynguyen.interceptors.interceptor.AdminInterceptor;
+import duynguyen.interceptors.interceptor.LogInterceptor;
+import duynguyen.interceptors.interceptor.OldLoginInterceptor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+@Configuration
+public class WebMvcConfig extends WebMvcConfigurerAdapter {
+
+    //
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        // LogInterceptor apply to all URLs.
+        registry.addInterceptor(new LogInterceptor());
+
+        // Old Login url, no longer use.
+        // Use OldURLInterceptor to redirect to a new URL.
+        registry.addInterceptor(new OldLoginInterceptor())//
+                .addPathPatterns("/admin/oldLogin");
+
+        // This interceptor apply to URL like /admin/*
+        // Exclude /admin/oldLogin
+        registry.addInterceptor(new AdminInterceptor())//
+                .addPathPatterns("/admin/*")//
+                .excludePathPatterns("/admin/oldLogin");
+    }
+
+}
